bffb4bbe376f73f72f58f277504527813be91c1b
alvarogarcia1010 <00111317@uca.edu.sv>
2019-05-31 14:34:18 -0600

    Implementando retrofit

--- app/src/main/java/com/petrlr14/mvvm/Services/Retrofit/GithubService.kt
+++ app/src/main/java/com/petrlr14/mvvm/Services/Retrofit/GithubService.kt
@@ -1,3 +1,5 @@
+const val GITHUB_API_BASE_URL = "https://api.github.com/"
+
 interface GithubService{
 
     @GET("/users/{user}/repos")
@@ -6,6 +8,7 @@ interface GithubService{
     companion object {
         fun getGitHubService():GithubService{
             return Retrofit.Builder()
+                .baseUrl(GITHUB_API_BASE_URL)
                 .addConverterFactory(MoshiConverterFactory.create())
                 .addCallAdapterFactory(CoroutineCallAdapterFactory())
                 .build().create(GithubService::class.java)
